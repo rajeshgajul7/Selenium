365b3a920a8f866c5031c060ad2380db2e8b156b
RajeshGajul <RajeshGajul@DESKTOP-7R6N9JA.in.ibm.com>
2020-02-27 19:47:55 +0530

    check first class

---
+++ Test_Project/src/com/javatpoint/corejava/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.javatpoint.corejava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("hello");
+
+	}
+
+}
